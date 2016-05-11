ee58b7e0c48f1f1d4fd664b701830fc5c4399ab2
YUE <389565326@qq.com>
2016-05-11 11:51:20 +0800

    Initial Commit

---
+++ BPWeiXin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BPWeiXin
+//
+//  Created by bingcai on 16/5/11.
+//  Copyright © 2016年 bingcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
